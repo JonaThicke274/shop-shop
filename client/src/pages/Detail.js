b103fa17de912a1676b14d8cec32df1822f9c52e
JonaThicke274 <lipatajonathan274@gmail.com>
2022-12-19 23:33:57 -0500

    refactored Details.js to use globalstate.js while still utilizing GraphQL useQuery just in case global state doesnt have products yet

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,50 +1,62 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-
+import { useStoreContext } from "../utils/GlobalState";
+import { UPDATE_PRODUCTS } from "../utils/actions";
 import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
 
 function Detail() {
-  const { id } = useParams();
+	// First call useStoreContext to retrieve current state from globalstate and dispatch method to update it
+	const [state, dispatch] = useStoreContext();
+	// Destructures id from params 
+	const { id } = useParams();
 
-  const [currentProduct, setCurrentProduct] = useState({});
+	const [currentProduct, setCurrentProduct] = useState({})
+	
+	const { loading, data } = useQuery(QUERY_PRODUCTS);
+	// Only products needed, so it's destructured from state
+	const { products } = state;
+	
+	// Checks if there is data in global state's products array and displays the one with the matching id
+	// If no products in global state are saved yet, uses product data from useQuery to set product data to global state object
+	// After the above is complete, it runs over again, but this time with data from products array and then runs setCurrentProduct()
+	useEffect(() => {
+		if (products.length) {
+			setCurrentProduct(products.find(product => product._id === id));
+		} else if (data) {
+			dispatch({
+				type: UPDATE_PRODUCTS,
+				products: data.products
+			});
+		}
+	}, [products, data, dispatch, id]);
 
-  const { loading, data } = useQuery(QUERY_PRODUCTS);
+	return (
+		<>
+			{currentProduct ? (
+				<div className="container my-1">
+				<Link to="/">← Back to Products</Link>
 
-  const products = data?.products || [];
+				<h2>{currentProduct.name}</h2>
 
-  useEffect(() => {
-    if (products.length) {
-      setCurrentProduct(products.find((product) => product._id === id));
-    }
-  }, [products, id]);
+				<p>{currentProduct.description}</p>
 
-  return (
-    <>
-      {currentProduct ? (
-        <div className="container my-1">
-          <Link to="/">← Back to Products</Link>
+				<p>
+					<strong>Price:</strong>${currentProduct.price}{' '}
+					<button>Add to Cart</button>
+					<button>Remove from Cart</button>
+				</p>
 
-          <h2>{currentProduct.name}</h2>
-
-          <p>{currentProduct.description}</p>
-
-          <p>
-            <strong>Price:</strong>${currentProduct.price}{' '}
-            <button>Add to Cart</button>
-            <button>Remove from Cart</button>
-          </p>
-
-          <img
-            src={`/images/${currentProduct.image}`}
-            alt={currentProduct.name}
-          />
-        </div>
-      ) : null}
-      {loading ? <img src={spinner} alt="loading" /> : null}
-    </>
-  );
-}
+				<img
+					src={`/images/${currentProduct.image}`}
+					alt={currentProduct.name}
+				/>
+				</div>
+			) : null}
+			{loading ? <img src={spinner} alt="loading" /> : null}
+		</>
+	);
+	}
 
 export default Detail;
