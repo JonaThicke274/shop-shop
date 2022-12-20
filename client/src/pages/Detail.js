4657d8df6a9ea76a8f201a68778627294ffb5254
JonaThicke274 <lipatajonathan274@gmail.com>
2022-12-20 00:38:59 -0500

    implemented functionality to remove items and update item quantity in cart

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -23,7 +23,7 @@ function Detail() {
 	
 	const { loading, data } = useQuery(QUERY_PRODUCTS);
 	// Only products needed, so it's destructured from state
-	const { products } = state;
+	const { products, cart } = state;
 	
 	// Checks if there is data in global state's products array and displays the one with the matching id
 	// If no products in global state are saved yet, uses product data from useQuery to set product data to global state object
@@ -40,9 +40,26 @@ function Detail() {
 	}, [products, data, dispatch, id]);
 
 	const addToCart = () => {
+		const itemInCart = cart.find((cartItem) => cartItem._id === id);
+	  
+		if (itemInCart) {
+			dispatch({
+				type: UPDATE_CART_QUANTITY,
+				_id: id,
+				purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+			});
+		} else {
+			dispatch({
+				type: ADD_TO_CART,
+				product: { ...currentProduct, purchaseQuantity: 1 }
+			});
+		}
+	};
+
+	const removeFromCart = () => {
 		dispatch({
-			type: ADD_TO_CART,
-			product: { ...currentProduct, purchaseQuantity: 1 }
+			type: REMOVE_FROM_CART,
+			_id: currentProduct._id
 		});
 	};
 
@@ -59,7 +76,12 @@ function Detail() {
 				<p>
 					<strong>Price:</strong>${currentProduct.price}{' '}
 					<button onClick={addToCart}>Add to Cart</button>
-					<button>Remove from Cart</button>
+					<button 
+						disabled={!cart.find(p => p._id === currentProduct._id)} 
+						onClick={removeFromCart}
+					>
+						Remove from Cart
+					</button>
 				</p>
 
 				<img
