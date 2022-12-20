2b4f91a15e5e8fc8a76f4da552a1abf8b2afc63e
JonaThicke274 <lipatajonathan274@gmail.com>
2022-12-19 23:09:59 -0500

    updated categoryMenu component to utilize global state reducers to update category and what products are displatyyed

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
 import { useQuery } from '@apollo/client';
 import { QUERY_CATEGORIES } from '../../utils/queries';
+import { useStoreContext } from "../../utils/GlobalState";
 
-function CategoryMenu({ setCategory }) {
-  const { data: categoryData } = useQuery(QUERY_CATEGORIES);
-  const categories = categoryData?.categories || [];
+function CategoryMenu() {
+	// First call useStoreContext to retrieve current state from globalstate and dispatch method to update it
+	const [state, dispatch] = useStoreContext();
+	// Only categories is needed so it's destructured from state
+	const { categories } = state;
 
-  return (
-    <div>
-      <h2>Choose a Category:</h2>
-      {categories.map((item) => (
-        <button
-          key={item._id}
-          onClick={() => {
-            setCategory(item._id);
-          }}
-        >
-          {item.name}
-        </button>
-      ))}
-    </div>
-  );
+	const { data: categoryData } = useQuery(QUERY_CATEGORIES);
+
+	// Runs immediately on load and then passes function to update global state and data we're dependent on(categoryData & dispatch)
+	// categoryData is undefined on load but useEffect sees  that categoryData exists and the nruns dispatch function
+	useEffect(() => {
+		// if categoryData exists or has changed from the response of useQuery, then run dispatch()
+		if (categoryData) {
+			// execute our dispatch function with our action object indicating the type of action and the data to set our state for categories to
+			dispatch({
+				type: UPDATE_CATEGORIES,
+				categories: categoryData.categories
+			});
+		}
+	}, [categoryData, dispatch])
+
+	// Runs dispatch to update current category
+	const handleClick = id => {
+		dispatch({
+			type: UPDATE_CURRENT_CATEGORY,
+			currentCategory: id
+		});
+	};
+
+	return (
+		<div>
+			<h2>Choose a Category:</h2>
+			{categories.map((item) => (
+				<button
+					key={item._id}
+					onClick={() => {
+						handleClick(item._id);
+					}}
+				>
+					{item.name}
+				</button>
+			))}
+		</div>
+	);
 }
 
 export default CategoryMenu;
