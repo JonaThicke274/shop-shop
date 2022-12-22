030a73322a0018c44e1bdfb7aa749fa671f3d883
JonaThicke274 <lipatajonathan274@gmail.com>
2022-12-21 19:35:36 -0500

    integrated stripe in mock shop/e-commerce app; however session ids are not properly created, name param missing but shouldn't be

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -6,9 +6,16 @@ import './style.css';
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/client';
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
     const [state, dispatch] = useStoreContext();
+    // Data variable will contain the  checkout session but only after the query is called with getCheckout
+    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
     
     // Checks to see if state.cart.length is 0 and then executes getCart()
     // ADD_MULTIPLE_TO_CART action is dispatched b/c we have an array of items returning from IndexedDB so all products can be dumped instead of 1-by-1
@@ -23,6 +30,14 @@ const Cart = () => {
         }
     }, [state.cart.length, dispatch]);
 
+    useEffect(() => {
+        if (data) {
+            stripePromise.then((res) => {
+                res.redirectToCheckout({ sessionId: data.checkout.session });
+            });
+        }
+    }, [data]);
+
     function toggleCart() {
         dispatch({ type: TOGGLE_CART });
     }
@@ -33,6 +48,16 @@ const Cart = () => {
             sum += item.price * item.purchaseQuantity;
         });
         return sum.toFixed(2);
+    }
+
+    function submitCheckout() {
+        const productIds = [];
+      
+        state.cart.forEach((item) => {
+            for (let i = 0; i < item.purchaseQuantity; i++) {
+                productIds.push(item._id);
+            }
+        });
     }
 
     if (!state.cartOpen) {
@@ -58,7 +83,7 @@ const Cart = () => {
                         <strong>Total: ${calculateTotal()}</strong>
                         {
                         Auth.loggedIn() ?
-                            <button>
+                            <button onClick={submitCheckout}>
                                 Checkout
                             </button>
                             :
